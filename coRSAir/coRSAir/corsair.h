46855bd4a5a62b4d2b2bf2ddb222a34d9a4fdd7f
iconalc <85000737+iconalc@users.noreply.github.com>
2022-07-15 13:34:08 +0200

    Add files via upload

---
+++ coRSAir/coRSAir/corsair.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corsair.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icondado <icondado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 16:22:54 by icondado          #+#    #+#             */
+/*   Updated: 2022/07/15 12:55:21 by icondado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CORSAIR_H
+# define CORSAIR_H
+
+# include <openssl/pem.h>
+# include <openssl/rsa.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int	corsair(void);
+
+#endif
