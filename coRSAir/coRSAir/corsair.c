a5abc95896f1165f46a9271c672d935b0bf487c9
iconalc <85000737+iconalc@users.noreply.github.com>
2022-07-18 18:26:55 +0200

    Add files via upload

--- coRSAir/coRSAir/corsair.c
+++ coRSAir/coRSAir/corsair.c
@@ -6,47 +6,88 @@
 /*   By: icondado <icondado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 12:57:01 by icondado          #+#    #+#             */
-/*   Updated: 2022/07/15 13:25:34 by icondado         ###   ########.fr       */
+/*   Updated: 2022/07/18 18:18:41 by icondado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corsair.h"
 
-char corsair(char *cert-file)
+char *corsair(const char *cert_filestr)
 {
-    const char cert_filestr[] = "./cert-file.pem";     
+       
+        EVP_PKEY *pkey = NULL;
+        BIO *certbio = NULL;
+        BIO *outbio = NULL;
+        X509 *cert = NULL;
+        int ret;
+
+        /* ---------------------------------------------------------- *
+         * These function calls initialize openssl for correct work.  *
+         * ---------------------------------------------------------- */
+        OpenSSL_add_all_algorithms();
+        ERR_load_BIO_strings();
+        ERR_load_crypto_strings();
+
+        /* ---------------------------------------------------------- *
+         * Create the Input/Output BIO's.                             *
+         * ---------------------------------------------------------- */
+        certbio = BIO_new(BIO_s_file());
+        outbio = BIO_new_fp(stdout, BIO_NOCLOSE);
+
+        /* ---------------------------------------------------------- *
+         * Load the certificate from file (PEM).                      *
+         * ---------------------------------------------------------- */
+        ret = BIO_read_filename(certbio, cert_filestr);
+        if (!(cert = PEM_read_bio_X509(certbio, NULL, 0, NULL)))
+        {
+                BIO_printf(outbio, "Error loading cert into memory\n");
+                exit(-1);
+        }
+
+        /* ---------------------------------------------------------- *
+         * Extract the certificate's public key data.                 *
+         * ---------------------------------------------------------- */
+        if ((pkey = X509_get_pubkey(cert)) == NULL)
+                BIO_printf(outbio, "Error getting public key from certificate");
+
+        /* ---------------------------------------------------------- *
+         * Print the public key information and the key in PEM format *
+         * ---------------------------------------------------------- */
+        /* display the key type and size here */
+        if (!PEM_write_bio_PUBKEY(outbio, pkey))
+                BIO_printf(outbio, "Error writing public key data in PEM format");
+
+        /* ---------------------------------------------------------- *
+         *                                                            *
+         * ---------------------------------------------------------- */
+        RSA* rsa = EVP_PKEY_get1_RSA(pkey);
+        
+        const BIGNUM* mod= RSA_get0_n(rsa);
+        const BIGNUM* exp= RSA_get0_e(rsa);
+
+        printf("\nMódulo Hexadecimal: \n");
+        BN_print_fp(stdout, mod);
+        printf("\nExponente Hexadecimal: \n");
+        BN_print_fp(stdout, exp);
+
+        
+        printf("\nMódulo:\n%s ", BN_bn2dec(mod));
+        printf("\nExponente:\n%s ", BN_bn2dec(exp));
+               
+        
+
+        /*free*/
+        EVP_PKEY_free(pkey);        
+        X509_free(cert);
+        BIO_free_all(certbio);
+        BIO_free_all(outbio);
+        return(0);
+        
 }
 
-int main(int argc, char **argv)
- {
-    unsigned char buffer[BUFSIZ];
-    FILE *f;
-    SHA_CTX ctx;
-    size_t len;
-    if (argc < 2) {
-            fprintf(stderr, "usage: %s <file>\n", argv[0]);
-            return 1;
-    }
-
-    f = fopen(argv[1], "r");
-    if (!f) {
-            fprintf(stderr, "couldn't open %s\n", argv[1]);
-            return 1;
-    }
-
-    SHA1_Init(&ctx);
-
-    do {
-            len = fread(buffer, 1, BUFSIZ, f);
-            SHA1_Update(&ctx, buffer, len);
-    } while (len == BUFSIZ);
-
-    SHA1_Final(buffer, &ctx);
-
-    fclose(f);
-
-    for (len = 0; len < SHA_DIGEST_LENGTH; ++len)
-            printf("%02x", buffer[len]);
-    putchar('\n');
-    return 0;
- }
+int main(void)
+{
+        const char cert_filestr[] = "cert.pem";
+        corsair(cert_filestr);
+        return(0);
+}
--- coRSAir/coRSAir/corsair.h
+++ coRSAir/coRSAir/corsair.h
@@ -6,18 +6,21 @@
 /*   By: icondado <icondado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 16:22:54 by icondado          #+#    #+#             */
-/*   Updated: 2022/07/15 12:55:21 by icondado         ###   ########.fr       */
+/*   Updated: 2022/07/18 18:19:22 by icondado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CORSAIR_H
 # define CORSAIR_H
 
+# include <openssl/bio.h>
+# include <openssl/err.h>
 # include <openssl/pem.h>
+# include <openssl/x509.h>
 # include <openssl/rsa.h>
 # include <stdio.h>
 # include <unistd.h>
 
-int	corsair(void);
+char *corsair(const char *cert_filestr);
 
 #endif
